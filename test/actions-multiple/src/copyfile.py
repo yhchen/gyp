77068fc9eb8e1c418609dfbf74a639ffa7e38681
yhchen <myself559@hotmail.com>
2019-03-11 11:42:07 +0800

    Update Reversion:
    commit	8bee09f4a57807136593ddc906b0b213c21f9014	[log] [tgz]
    author	Jared Wyles <jared.wyles@gmail.com>	Wed Feb 27 07:10:46 2019
    committer	Mark Mentovai <mark@chromium.org>	Fri Mar 01 22:21:49 2019

--- test/actions-multiple/src/copyfile.py
+++ test/actions-multiple/src/copyfile.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-
-# Copyright (c) 2009 Google Inc. All rights reserved.
+# Copyright (c) 2011 Google Inc. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import shutil
 import sys
 
-contents = "Hello from make-file.py\n"
-
-open(sys.argv[1], 'w').write(contents)
+shutil.copyfile(sys.argv[1], sys.argv[2])
